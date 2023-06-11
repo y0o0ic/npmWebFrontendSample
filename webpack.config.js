cf4d4715c6f507105cc842ac38f7bae1aa115fe5
Yasuo Ichinose <y0o0ic@gmail.com>
2023-06-11 15:11:53 +0200

    updaet webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
 
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: "production", //"development", //production
+  // mode: "development",
+  mode: "production",
 
   // ローカル開発用環境を立ち上げる
   // 実行時にブラウザが自動的に localhost を開く
@@ -20,4 +21,9 @@ module.exports = {
     static: "dist",
     open: true,
   },
+  // other webpack configuration options...
+  performance: {
+    maxAssetSize: 100000000000, // Adjust the value as needed or set to 0 to disable the warning
+  },
 };
+
